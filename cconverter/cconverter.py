315abea092ac5edd0d5e992be2f0aedde1fcfc3d
Tanawa Tsamo Marius <mariustanawa@gmail.com>
2022-03-02 11:30:16 +0100

    Stage 4/6

--- cconverter/cconverter.py
+++ cconverter/cconverter.py
@@ -1,22 +1,14 @@
-def get_coins() -> int:
-    return int(input("Please, enter the number of conicoins you have:"))
-
-
-def get_rate_exchange() -> int or float:
-    rate_ = input("Please, enter the exchange rate:")
-    try:
-        result = int(rate_)
-    except ValueError:
-        result = float(rate_)
-
-    return result
-
-
-def print_the_value_dollars(dollars) -> None:
-    print("The total amount of dollars:", dollars)
-
-
 if __name__ == '__main__':
-    coins = get_coins()
-    rate = get_rate_exchange()
-    print_the_value_dollars(coins * rate)
+
+    conicoins_converter = {
+        "RUB": 2.98,
+        "ARS": 0.82,
+        "HNL": 0.17,
+        "AUD": 1.9622,
+        "MAD": 0.208
+    }
+
+    coins = input()
+    coins = float(coins)
+    for currency, rate in conicoins_converter.items():
+        print(f'I will get {round(coins * rate, 2)} {currency} from the sale of {coins} conicoins.')
