b0ff3c1aa9c8ec46ccd94253f8ad1e967a6f75e4
Tanawa Tsamo Marius <mariustanawa@gmail.com>
2022-03-02 11:15:42 +0100

    Stage 3/6

--- cconverter/cconverter.py
+++ cconverter/cconverter.py
@@ -1,7 +1,22 @@
-if __name__ == '__main__':
-    conicoins = int(input())
+def get_coins() -> int:
+    return int(input("Please, enter the number of conicoins you have:"))
 
-    converter = dict(conicoins=100)
-    print(f'I have {conicoins} conicoins.')
-    print(f'{conicoins} conicoins cost {conicoins * converter["conicoins"]} dollars.')
-    print('I am rich! Yippee!')
+
+def get_rate_exchange() -> int or float:
+    rate_ = input("Please, enter the exchange rate:")
+    try:
+        result = int(rate_)
+    except ValueError:
+        result = float(rate_)
+
+    return result
+
+
+def print_the_value_dollars(dollars) -> None:
+    print("The total amount of dollars:", dollars)
+
+
+if __name__ == '__main__':
+    coins = get_coins()
+    rate = get_rate_exchange()
+    print_the_value_dollars(coins * rate)
