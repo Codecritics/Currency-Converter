eeecdf3fb58256b42a5c3669065d7616c32407f6
Tanawa Tsamo Marius <mariustanawa@gmail.com>
2022-03-02 11:03:24 +0100

    Stage 2/6

--- cconverter/cconverter.py
+++ cconverter/cconverter.py
@@ -1,1 +1,7 @@
-print("Meet a conicoin!")
+if __name__ == '__main__':
+    conicoins = int(input())
+
+    converter = dict(conicoins=100)
+    print(f'I have {conicoins} conicoins.')
+    print(f'{conicoins} conicoins cost {conicoins * converter["conicoins"]} dollars.')
+    print('I am rich! Yippee!')
